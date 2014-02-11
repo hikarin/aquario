c2b8d930cb009f6f7a0a1c0df5f51e17260042d7
Hikari Aikawa <hikari@hikar.in>
2014-02-12 00:43:39 +0900

    test skips generationa GC(temporarily)

--- run-test.sh
+++ run-test.sh
@@ -2,4 +2,4 @@ make opt
 time ./aquario -GC copying tak.lsp
 time ./aquario -GC mark_compact tak.lsp
 time ./aquario -GC reference_count tak.lsp
-time ./aquario -GC generational tak.lsp
+#time ./aquario -GC generational tak.lsp
