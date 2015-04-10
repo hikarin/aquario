c5654b515862610fa8284239f537601c04e9ccec
Hikari Aikawa <hikari@hikar.in>
2015-04-10 12:45:41 +0900

    prof with all collectors

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,16 @@
 make prof
-#time ./aquario -GC copying tak.lsp
-#time ./aquario -GC mark_compact tak.lsp
+
+#copying
+time ./aquario -GC copying tak.lsp
+gprof ./aquario gmon.out > analysis.txt
+cat analysis.txt
+
+#mark_compact
+time ./aquario -GC mark_compact tak.lsp
+gprof ./aquario gmon.out > analysis.txt
+cat analysis.txt
+
+#generational
 time ./aquario -GC generational tak.lsp
 gprof ./aquario gmon.out > analysis.txt
 cat analysis.txt
