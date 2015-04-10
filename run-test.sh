5a2b69f03a08c451e42d3b60d53348404f0d12c7
Hikari Aikawa <hikari@hikar.in>
2015-04-10 12:53:04 +0900

    prof

--- run-test.sh
+++ run-test.sh
@@ -1,16 +1,16 @@
-make prof
+make opt
 
 #copying
 time ./aquario -GC copying tak.lsp
-gprof ./aquario gmon.out > analysis.txt
-cat analysis.txt
+#gprof ./aquario gmon.out > analysis.txt
+#cat analysis.txt
 
 #mark_compact
 time ./aquario -GC mark_compact tak.lsp
-gprof ./aquario gmon.out > analysis.txt
-cat analysis.txt
+#gprof ./aquario gmon.out > analysis.txt
+#cat analysis.txt
 
 #generational
 time ./aquario -GC generational tak.lsp
-gprof ./aquario gmon.out > analysis.txt
-cat analysis.txt
+#gprof ./aquario gmon.out > analysis.txt
+#cat analysis.txt
