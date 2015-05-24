21ee246dd3c2e3719f3b7c2864a62d0207dc6404
Hikari Aikawa <hikari@hikar.in>
2015-05-24 15:27:59 +0900

    removed redundant comments

--- run-test.sh
+++ run-test.sh
@@ -2,15 +2,9 @@ make opt
 
 #copying
 time ./aquario -GC copying tak.lsp
-#gprof ./aquario gmon.out > analysis.txt
-#cat analysis.txt
 
 #mark_compact
 time ./aquario -GC mark_compact tak.lsp
-#gprof ./aquario gmon.out > analysis.txt
-#cat analysis.txt
 
 #generational
 time ./aquario -GC generational tak.lsp
-#gprof ./aquario gmon.out > analysis.txt
-#cat analysis.txt
