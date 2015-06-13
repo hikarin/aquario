9817765308e4232869f79026e29ce6201b3fd38c
Hikari Aikawa <hikari@hikar.in>
2015-06-14 00:04:51 +0900

    separation of each benchmark

--- run-test.sh
+++ run-test.sh
@@ -1,13 +1,14 @@
 make opt
 
 #copying
-time ./aquario -GC copy tak.lsp
-
+time ./aquario -GC copy fib.lsp
+echo =======================
 #mark_compact
-time ./aquario -GC mc tak.lsp
-
+time ./aquario -GC mc fib.lsp
+echo =======================
 #generational
-time ./aquario -GC gen tak.lsp
-
+time ./aquario -GC gen fib.lsp
+echo =======================
 #reference_count
-time ./aquario -GC ref tak.lsp
+time ./aquario -GC ref fib.lsp
+echo =======================
