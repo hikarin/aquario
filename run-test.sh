14e5b4eca7630a36a22debc786716def2a779596
Hikari Aikawa <hikari@hikar.in>
2016-02-27 10:28:45 +0900

    easy to modify script

--- run-test.sh
+++ run-test.sh
@@ -1,17 +1,19 @@
 make opt
 
-#mark_sweep
-time ./aquario -GC ms tak.lsp
+SCRIPT=test/tak.lsp
 
+#mark_sweep
+time ./aquario -GC ms $SCRIPT
+echo =======================
 #copying
-time ./aquario -GC copy test/fib.lsp
+time ./aquario -GC copy $SCRIPT
 echo =======================
 #mark_compact
-time ./aquario -GC mc test/fib.lsp
+time ./aquario -GC mc $SCRIPT
 echo =======================
 #generational
-time ./aquario -GC gen test/fib.lsp
+time ./aquario -GC gen $SCRIPT 
 echo =======================
 #reference_count
-time ./aquario -GC ref test/fib.lsp
+time ./aquario -GC ref $SCRIPT
 echo =======================
