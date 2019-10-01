09d6088ee2c81e251305957686a5844480d7ed83
Hikari <hicalin@gmail.com>
2019-09-30 21:37:22 -0400

    put a name to magic numbers

--- test.c
+++ test.c
@@ -1,12 +1,15 @@
 #include <string.h>
 
+#define TEST_RESULT_SUCCESS (0)
+#define TEST_RESULT_FALSE   (1)
+
 int main(int argc, char *argv[])
 {
     if(argc != 3)
-	return 1;
+	return TEST_RESULT_FALSE;
 
-    if(strcmp(argv[1], argv[2]) == 0)
-	return 0;
+    if(strcmp(argv[1], argv[2]) != 0)
+	return TEST_RESULT_FALSE;
     
-    return 1;
+    return TEST_RESULT_SUCCESS;
 }
